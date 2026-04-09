5ad93dcc2361b10eed63b02b0573a17cec956be4
Mr-Sanjay-3 <sanjayprogrammer3@gmail.com>
2026-04-09 11:52:19 +0300

    fix: vite.config2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const baseURL=import.meta.env.VITE_BASEURL;
+const baseURL= process.env.VITE_BASEURL;
 
 // https://vite.dev/config/
 export default defineConfig({
