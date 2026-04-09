5f5df5f6cdfe64d769ddb784b3850462caed8bd9
Mr-Sanjay-3 <sanjayprogrammer3@gmail.com>
2026-04-09 11:48:19 +0300

    fix: vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const baseURL=import.meta.env.VITE_BASEURL;
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_BASEURL,
+        target: baseURL,
         changeOrigin: true,
       },
     },
